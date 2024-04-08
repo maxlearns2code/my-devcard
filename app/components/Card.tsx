cf68810768a2fd10a7e4f8f2b0a07f2fe7fd30a5
Maxime <maxdevvynter@gmail.com>
2024-04-08 11:20:52 +0200

    add profile picture

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
+
 export default function Card() {
   return (
     <div className="w-full max-w-[324px] aspect-card rounded-4xl relative overflow-hidden">
       <div className="absolute inset-4 rounded-4xl bg-white flex flex-col">
-        <div className="flex-1 border-8 rounded-4xl border-gray-300 bg-pattern bg-cover"></div>
+        <div className="flex-1 border-8 rounded-4xl border-gray-300 bg-pattern bg-cover relative">
+          <Image
+            src="/assets/portrait.jpg"
+            alt="profile picture"
+            width={150}
+            height={150}
+            objectFit="cover"
+            className="rounded-[48px] border-8 border-white -rotate-3 absolute left-[3%] top-[3%]"
+          />
+        </div>
         <div className="flex-1"></div>
       </div>
       <style jsx>{`
