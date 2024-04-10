c2233dd6085f2bc3578075f6434d07362f504fe2
Maxime <maxdevvynter@gmail.com>
2024-04-10 18:47:39 +0200

    refacto

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -2,6 +2,10 @@ import { motion, useMotionValue, useSpring, useTransform } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import data from "../lib/data.json";
 import Icons from "./Icons";
 
 export default function Card() {
@@ -15,6 +19,7 @@ export default function Card() {
   const rotateY = useTransform(x, [0, 356], [15, -15]);
   const rotateXSpring = useSpring(rotateX);
   const rotateYSpring = useSpring(rotateY);
+  const tags = data.tags;
   return (
     <motion.div
       className="w-full max-w-[356px] aspect-card rounded-4xl relative overflow-hidden"
@@ -36,7 +41,7 @@ export default function Card() {
         glareY.set(-1000);
       }}
       ref={cardRef}
-      style={{ rotateY: rotateYSpring, rotateX: rotateXSpring }}
+      style={{ rotateX: rotateXSpring, rotateY: rotateYSpring }}
     >
       <motion.div
         className="bg-gradient-radial from-white from-20% to-white/0 to-70% opacity-25 absolute size-[600px]"
@@ -50,51 +55,60 @@ export default function Card() {
             alt="profile picture"
             width={150}
             height={150}
-            objectFit="cover"
-            loading="lazy"
-            className="rounded-[48px] border-8 border-white -rotate-3 absolute left-[3%] top-[3%]"
+            style={{ objectFit: "cover" }}
+            className="rounded-[48px] border-8 border-white -rotate-3 absolute left-[4%] top-[4%]"
           />
-          <div className="bg-gray-900 shadow-[rgba(0,0,0,0.8)_1px_1px_15px] p-4 absolute bottom-0 inset-x-0 text-gray-500 text-xs rounded-2xl translate-y-1/2 flex space-x-4">
+          <div className="bg-gray-900 shadow-[rgba(0,0,0,0.8)_1px_1px_15px] py-2 px-4 absolute bottom-0 inset-x-0 text-white text-xs rounded-2xl translate-y-1/2 flex space-x-4">
             <span className="space-x-2 flex items-center">
-              <b className="text-white text-lg">12</b>
-              <span className="line-clamp-1">Projects</span>
+              <b className="text-lg">12</b>
+              <span className="line-clamp-1">Repositories</span>
             </span>
             <span className="space-x-2 flex items-center">
-              <b className="text-white text-lg">250</b>
+              <b className="text-lg">250</b>
               <span className="line-clamp-1">Contributions</span>
             </span>
           </div>
         </div>
         <div className="flex-1 text-black rounded-4xl px-2 pb-2">
           <div className="shadow-[rgba(0,0,0,0.3)_1px_1px_1px] h-full rounded-4xl px-4 relative overflow-hidden">
-            <div className="pt-8">
-              <h1 className="text-2xl font-bold">Max Dewynter</h1>
-              <div className=" text-xs space-x-2">
-                <span>@maxlearns2code</span>
-                <span className="text-gray-400">2024</span>
+            <div className="pt-6">
+              <h1 className="text-2xl font-bold text-gray-900">{data.name}</h1>
+              <div className=" text-xs flex gap-3 text-gray-900">
+                <b>{data.role}</b>
+                <div className="flex items-center gap-1">
+                  <FaLocationDot color="rgb(17 24 39 / var(--tw-text-opacity))" />
+                  <span className="text-xs">{data.location}</span>
+                </div>
+              </div>
+              <div className=" text-xs">
+                <span>{data.description}</span>
               </div>
             </div>
             <hr className="my-2" />
-            <div className="text-xs flex flex-wrap gap-2">
-              {["#javascript", "#ts", "#react", "#nextjs", "#webdev"].map(
-                (tag) => (
-                  <span
-                    key={tag}
-                    className="border border-black px-2 py-1 rounded-lg"
-                  >
-                    {tag}
-                  </span>
-                )
-              )}
+            <div className="text-xs flex flex-wrap gap-1">
+              {tags.map((e) => (
+                <span
+                  key={e.id}
+                  className="border border-black px-2 py-1 rounded-lg"
+                >
+                  {e.tag}
+                </span>
+              ))}
             </div>
             <div>
-              <div className="w-[50%] absolute bottom-0 left-0 bg-red rounded-tr-4xl p-4 flex justify-between">
+              <div className="w-[50%] absolute bottom-0 left-0 bg-red rounded-tr-4xl px-4 py-3 grid grid-cols-5 gap-1">
+                <Icons name="javascript" className="h-[18px] w-[18px]" />
                 <Icons name="react" className="h-[18px] w-[18px]" />
                 <Icons name="next" className="h-[18px] w-[18px]" />
                 <Icons name="typescript" className="h-[18px] w-[18px]" />
+                <Icons name="redux" className="h-[18px] w-[18px]" />
+                <Icons name="sass" className="h-[18px] w-[18px]" />
                 <Icons name="tailwind" className="h-[18px] w-[18px]" />
+                <Icons name="figma" className="h-[18px] w-[18px]" />
+                <Icons name="jest" className="h-[18px] w-[18px]" />
+                <Icons name="framer" className="h-[18px] w-[18px]" />
               </div>
-              <div className="w-[50%] absolute bottom-0 right-0 bg-gray-900 text-white rounded-tl-4xl p-4 flex justify-between">
+              <div className="w-[50%] absolute bottom-0 right-0 bg-gray-900 text-white rounded-tl-2xl py-3 px-4 flex justify-between">
                 <Link
                   tabIndex={0}
                   aria-label="take a look at my linkedin profile"
@@ -102,7 +116,10 @@ export default function Card() {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <Icons name="linkedin" className="h-[18px] w-[18px]" />
+                  <div className=" cursor-pointer hover:scale-110 ease-in duration-300">
+                    <FaLinkedinIn />
+                    <span className="sr-only">Linkedin link</span>
+                  </div>
                 </Link>
                 <Link
                   tabIndex={0}
@@ -111,7 +128,10 @@ export default function Card() {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <Icons name="github" className="h-[18px] w-[18px]" />
+                  <div className=" cursor-pointer hover:scale-110 ease-in duration-300">
+                    <FaGithub />
+                    <span className="sr-only">Github link</span>
+                  </div>
                 </Link>
                 <Link
                   tabIndex={0}
@@ -120,7 +140,10 @@ export default function Card() {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <Icons name="mail" className="h-[18px] w-[18px]" />
+                  <div className=" cursor-pointer hover:scale-110 ease-in duration-300">
+                    <AiOutlineMail />
+                    <span className="sr-only">Email link</span>
+                  </div>
                 </Link>
               </div>
             </div>
---
+++ app/components/Tags.tsx
@@ -0,0 +1,7 @@
+interface ProjectsitemsProps {
+  tag: string;
+}
+
+export default function Tags({ tag }: ProjectsitemsProps) {
+  return <p>{tag}</p>;
+}
